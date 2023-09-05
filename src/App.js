4621056ec856f1d5ae093d399afc70046c2c6703
Kwashie Abednego <kwashieabednego11@gmail.com>
2023-09-05 23:11:19 +0000

    Counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import {ChevronUp, ChevronsUp, RotateCcw, Hash,ChevronsDown,ChevronDown,} from "react-feather";
 
 const App = () => {
