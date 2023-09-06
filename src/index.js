de19f10836ff8eb38471b0a5e3af98d279d07e35
Kwashie Abednego <kwashieabednego11@gmail.com>
2023-09-06 16:48:21 +0000

    Counter update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as Icon from "react-feather";
+import * as  Icon from "react-feather";
 import "./index.css";
 import App from "./App";
 import "./App.css";
